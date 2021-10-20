757b8aff404b121970e948633111eb9dba60cae8
unknown <youssef>
2021-10-20 14:10:52 +0200

    lugar y ano de nacimiento

--- prueba.java
+++ prueba.java
@@ -1,8 +1,9 @@
-public class MiPrimeraClase {
+public class prueba {
 
 	public static void main(String args[]) {
 
 		System.out.println("YOUSSEF MOUHOUB");
+		System.out.println("MARRUECOS, 2001");
 
 	}
 
