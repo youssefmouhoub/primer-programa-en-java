86e3db6951843f9fff2fa466ca3623160f5f7d77
youssef <youssefleon75rgmail.com>
2021-10-22 09:45:16 +0200

    cantamte fav

--- prueba.java
+++ prueba.java
@@ -2,9 +2,9 @@ public class prueba {
 
 	public static void main(String args[]) {
 
-		System.out.println("YOUSSEF MOUHOUB");
 		System.out.println("MARRUECOS, 2001");
 		System.out.println("leon, espana");
+		System.out.println("nf");
 
 	}
 
